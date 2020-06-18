b585a0baa040e7c62136d473f8f57ea01e4e734c
Contributor @ WLI <38062303+partners-wli@users.noreply.github.com>
2020-06-18 15:03:51 +0530

    Update index.vue

--- index.vue
+++ index.vue
@@ -1,1 +1,16 @@
+<template>
+  <div>
+  <VueImageUpload/>
+  </div>
+</template>
 
+<script>
+// import vue-weblineindia-image-crop-upload
+import VueImageUpload from 'vue-weblineindia-image-crop-upload'
+
+export default {
+  components: {
+    VueImageUpload,
+  },
+}
+</script>
